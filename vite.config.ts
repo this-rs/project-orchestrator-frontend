22f7cc5f31e9bb656568f6a68ab57b8edcd5f25e
Theotime Riviere <t@ffs.land>
2026-02-10 03:25:58 +0100

    fix: remove allowedhost use variable

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    allowedHosts: ['ffs.dev'],
+    allowedHosts: process.env.VITE_ALLOWED_HOSTS?.split(',') || true,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
